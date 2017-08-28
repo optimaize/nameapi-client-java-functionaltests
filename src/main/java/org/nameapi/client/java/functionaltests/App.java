54bb2845e7c44319fc6687513e38ce62463b5ccf
nicoletorres <thisIsNew14>
2017-08-28 14:52:33 +0300

    Initial commit

---
+++ src/main/java/org/nameapi/client/java/functionaltests/App.java
@@ -0,0 +1,13 @@
+package org.nameapi.client.java.functionaltests;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
